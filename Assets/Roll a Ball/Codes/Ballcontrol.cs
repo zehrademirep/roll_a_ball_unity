25611be3c705f9a0e8b8617ee2a8aa8d54507c69
Zehra Demir <zehrademir13@outlook.com>
2020-04-23 14:47:02 +0300

    roll_a_ball

---
+++ Assets/Roll a Ball/Codes/Ballcontrol.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ballcontrol : MonoBehaviour {
+
+    Rigidbody fizik;
+    public int hiz;
+    int sayac = 0;
+    public int cubicSayisi;
+    public Text sayacText;
+    public Text oyunbittiText;
+
+	void Start ()
+    {
+        fizik = GetComponent<Rigidbody>();
+	}
+	
+	
+	void FixedUpdate ()
+    {
+        float yatay = Input.GetAxisRaw("Horizontal");
+        float dikey = Input.GetAxisRaw("Vertical");
+
+        Vector3 vec = new Vector3(yatay, 0, dikey);
+        fizik.AddForce(vec*hiz);
+
+      //  Debug.Log("yatay = " +yatay    "dikey = " + dikey);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        /* if (other.gameObject.tag == "eat")
+         {
+             Destroy(other.gameObject);
+
+         }*/
+        other.gameObject.SetActive(false);
+        sayac++;
+
+        sayacText.text = "Sayac = " + sayac;
+
+        if(sayac==cubicSayisi)
+        {
+            oyunbittiText.text = "OYUN BİTTİ";
+        }
+            
+
+        
+
+        
+    }
+}
---
+++ Assets/Roll a Ball/Codes/Cameracontrol.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cameracontrol : MonoBehaviour {
+
+    public GameObject Ball;
+    Vector3 Range;
+	void Start ()
+    {
+        Range = transform.position - Ball.transform.position;
+	}
+	
+	
+	void LateUpdate ()
+    {
+        transform.position = Ball.transform.position + Range;
+	}
+}
